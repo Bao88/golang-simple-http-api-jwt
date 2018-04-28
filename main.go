c9d620a26e6567dff0be1008b9df1f73f515e4bf
HBN <ngohoangbao@outlook.com>
2018-04-29 01:17:23 +0200

    added routing features

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 // type Users struct {
@@ -13,19 +14,37 @@ import (
 
 // type Users []User
 
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome to the Homepage!")
-	fmt.Println("Endpoint Hit: homepage")
+// func homePage(w http.ResponseWriter, r *http.Request){
+// 	fmt.Fprintf(w, "Welcome to the Homepage!")
+// 	fmt.Println("Endpoint Hit: homepage")
+// }
+
+// func handleRequests() {
+// 	// http.HandleFunc("/", homePage)
+// 	// Serve the Invoice form to clients
+// 	http.Handle("/", http.FileServer(http.Dir("./views/invoice")))
+	
+// 	log.Fatal(http.ListenAndServe(":8081", nil))
+// }
+
+func UserLogin(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the UserLogin!")
+	fmt.Println("Implement UserLogin")
 }
 
-func handleRequests() {
-	// http.HandleFunc("/", homePage)
-	// Serve the Invoice form to clients
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	log.Fatal(http.ListenAndServe(":8081", nil))
+func CreateInvoice(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the CreateInvoice!")
+	fmt.Println("Implement CreateInvoice")
 }
 
 func main(){
 	// fmt.Printf("hello, world\n")
-	handleRequests()
+	fmt.Println("Started")
+	router := mux.NewRouter();
+	router.Handle("/", http.FileServer(http.Dir("./views/invoice")))
+	router.HandleFunc("/login", UserLogin).Methods("GET")
+	router.HandleFunc("/invoice", CreateInvoice).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", router))
+	
+	// handleRequests()
 }
